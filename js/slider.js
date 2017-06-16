c4c03c3686ec2f590fd65f64206860dedefdcc93
mavijose <ogoumavi@gmail.com>
2017-06-16 12:47:27 +0000

    tp2

---
+++ js/slider.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+ $('.bxslider').bxSlider({
+  minSlides: 2,
+ maxSlides: 3,
+  slideWidth: 360,
+  slideMargin: 10,
+  auto: true
+});
+});
+<section class="bxslider">
+   	 <ol class="bxslider">
+   	  <li><img src="images/car_Note_photo_1_-2_1131x803.png"></li>
+   	  <li><img src="images/movement_2-2_1131x803.png"></li>
+   	  <li><img src="images/movement_3_1131x803.png"></li>
+   	  <li><img src="images/movement_5_1131x803_vertical.png"></li>
+   	  
+   </ol>
+
+     <script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+     <script type="text/javascript" src="js/jquery.bxslider.js"></script>
+     <script type="text/javascript" src="js/slider.js"></script>
+     .header-products-link {
+    text-align: center;
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+.header-products .hscroll {
+    width: 1045px;
+}
+.hscroll {
+    overflow: hidden;
+     }
+
+
+
+    
